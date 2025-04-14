b3f6a123833774537fd8237afa31bb19e81dadcc
Reece Peardon <peardon.reece@gmail.com>
2025-04-13 22:30:05 -0700

    set up TMDB api

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+//const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+//const options = {
+//  method: 'GET',
+  //headers: {
+    //accept: 'application/json',
+    //Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWEzN2JjMzcxNzg1YjE0YTc2MDk0MjVlNjM5MzA0OCIsIm5iZiI6MTc0NDYwNzM5Mi43MTUwMDAyLCJzdWIiOiI2N2ZjOThhMDYxYjFjNGJiMzI5OTFmNGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ebp1rcpuyl3FUWfzwoGa8SdY4-l2uWlhBFmsuaZyUhU'
+  //}
+//};
+
+//fetch(url, options)
+  //.then(res => res.json())
+  //.then(json => console.log(json))
+  //.catch(err => console.error(err));
