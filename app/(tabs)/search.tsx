5a58b7055018a7a35b6af4403db7f18095a8a3c1
Reece Peardon <peardon.reece@gmail.com>
2025-05-04 12:56:10 -0700

    starting build of search tab

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
+import { images } from '@/constants/images'
 import React from 'react'
+import MovieCard from '@/components/MovieCard'
+import { useRouter } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
 
 const search = () => {
+    const router = useRouter()
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError } = useFetch(() => fetchMovies({
+            query: ''
+        }))
+
     return (
-        <View>
-            <Text>search</Text>
+        <View className='flex-1 bg-primary'>
+            <Image source={images.bg} className='flex-1 absolute w-full z-0'
+                resizeMode='cover' />
+            <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />} 
+                keyExtractor={(item) => item.id.toString()}
+                className='px-5'
+                numColumns={3}
+                columnWrapperStyle={{
+                    justifyContent: 'center',
+                    gap: 16,
+                    marginVertical: 16,
+                }}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            />
         </View>
     )
 }
